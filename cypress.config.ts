5cf1027fdef6477e0e5415a5522b380f6e426c14
MaksLykh <joke_night_crow@mail.ru>
2024-11-13 19:54:48 -0800

    base url fix

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl:"https://the-internet.herokuapp.com/javascript_alerts",
+    baseUrl:"https://the-internet.herokuapp.com",
     env:{
 
     },
---
+++ cypress/e2e/alert.cy.ts
@@ -0,0 +1,9 @@
+describe('ALERT', () => {
+  beforeEach(() => {
+    cy.visit('/javascript_alerts');
+  });
+
+  it('Click for JS Alert', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
