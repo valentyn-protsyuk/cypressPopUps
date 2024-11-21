29121576ba2fdf3a6896fb63544b9236576dcefa
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-11-20 20:14:37 -0800

    broken img test

--- cypress/e2e/brokenImg.cy.ts
+++ cypress/e2e/brokenImg.cy.ts
@@ -7,4 +7,14 @@ describe('BROKEN IMAGE', () => {
         cy.visit('/broken_images'); 
     });
     
+    it('First broken img', () => {
+        cy.get('div.example img')
+        .first()
+        .should('be.visible')
+        .and((el)=>{
+            img = el[0] as HTMLImageElement
+            expect(img.naturalHeight,'Img Height: ').to.eq(0)
+            expect(img.naturalWidth,'Img Width: ').to.eq(0)
+        })
+    });
 });
